72bcc3249524d1131e8df6e5fdd6f94fadf5a4a3
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-01-18 21:44:15 -0500

    using useState to obtain the values from the search input

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './ToDoSearch.css'
 
 function ToDoSearch() {
+  const [searchValue, setSearchValue] = useState(''); 
+
   const onSearchValueChange = (e) => {
-    console.log(e.target.value)
+    //console.log(e.target.value)
+    setSearchValue(e.target.value)
   }
 
-  return (
+  return [
+    <>
     <input 
       className="ToDoSearch" 
       placeholder="Tomar..."
+      value={searchValue}
       onChange={onSearchValueChange}
     />
-  );
+    <p>{searchValue}</p>
+    </>
+  
+  ];
 };
 
 export { ToDoSearch };
