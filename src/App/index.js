3acd31bbbe3a683f376f7a0e308c1d2b6ab97b20
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-01-19 21:11:26 -0500

    started using localStorage and created a function to maintain updated todos

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,33 @@
 import { useState } from "react";
 import { AppUI } from "./AppUI";
 
-const defaultToDos = [
-  {
-    text: 'Tomar las pastillas',
-    completed: false
-  },
-  {
-    text: 'Tomar agua',
-    completed: false
-  },
-  {
-    text: 'Estudiar en Platzi',
-    completed: true
-  }
-]
+// const defaultToDos = [
+//   {
+//     text: 'Tomar las pastillas',
+//     completed: false
+//   },
+//   {
+//     text: 'Tomar agua',
+//     completed: false
+//   },
+//   {
+//     text: 'Estudiar en Platzi',
+//     completed: true
+//   }
+// ]
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const [toDos, setToDos] = useState(defaultToDos);
+  const localStorageToDos = localStorage.getItem('TODOS_V1');
+  let parsedToDos;
+
+  if (!localStorageToDos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedToDos = [];
+  } else {
+    parsedToDos = JSON.parse(localStorageToDos);
+  }
+
+  const [toDos, setToDos] = useState(parsedToDos);
   const [searchValue, setSearchValue] = useState(''); 
 
   const completedToDos = toDos.filter(toDo => !!toDo.completed).length; //filtrando toDos para saber cuáles tienen la propiedad completed como true, para contarlos.
@@ -36,18 +45,25 @@ function App() {
     })
   }
 
+  //para persistir la información de nuestras funciones en el LS
+  const saveToDos = (newToDos) => {
+    const stringifiedToDos = JSON.stringify(newToDos);
+    localStorage.setItem('TODOS_V1', stringifiedToDos);
+    setToDos(newToDos);
+  }
+
   const toggleCompleteToDo = (text) => {
     const toDoIndex = toDos.findIndex(toDo => toDo.text === text);
     const newToDos = [...toDos];
     newToDos[toDoIndex].completed = !newToDos[toDoIndex].completed; //cambia el valor de completed a su negación (true a false o false a true)
-    setToDos(newToDos);
+    saveToDos(newToDos);
   }
 
   const deleteToDo = (text) => {
     const todoIndex = toDos.findIndex(toDo => toDo.text === text);
     const newToDos = [...toDos];
     newToDos.splice(todoIndex, 1);
-    setToDos(newToDos);
+    saveToDos(newToDos);
   };
   
   return (
